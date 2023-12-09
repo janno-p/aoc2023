f93885eef5b8fae6ba50270edffb5d3aa48f4db7
Janno Põldma <janno.poldma@gmail.com>
2023-12-09 13:56:31 +0200

    Day09

---
+++ day09/src/main.rs
@@ -0,0 +1,83 @@
+use std::io::BufRead;
+
+use aoc::{execute, get_reader, Result};
+
+/*
+*5*  10  13  16  21  30  45
+ *5*   3   3   5   9  15
+ *-2*   0   2   4   6
+ *2*   2   2   2
+ *0*   0   0
+*/
+
+fn part1() -> Result<i32> {
+    let mut reader = get_reader();
+    let mut buffer = String::new();
+
+    let mut sum = 0i32;
+
+    loop {
+        buffer.clear();
+        if reader.read_line(&mut buffer)? == 0 {
+            break;
+        }
+
+        let numbers: Vec<_> = buffer
+            .trim()
+            .split(' ')
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+
+        let mut differences = Vec::new();
+        for number in numbers {
+            differences.push(number);
+            let mut last = *differences.last().unwrap();
+            for difference in differences.iter_mut().rev().skip(1) {
+                last -= *difference;
+                *difference = last;
+            }
+        }
+
+        sum += differences.iter().sum::<i32>();
+    }
+
+    Ok(sum)
+}
+
+fn part2() -> Result<i32> {
+    let mut reader = get_reader();
+    let mut buffer = String::new();
+
+    let mut sum = 0i32;
+
+    loop {
+        buffer.clear();
+        if reader.read_line(&mut buffer)? == 0 {
+            break;
+        }
+
+        let numbers: Vec<_> = buffer
+            .trim()
+            .split(' ')
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+
+        let mut differences = Vec::new();
+        for &number in numbers.iter().rev() {
+            differences.push(number);
+            let mut last = *differences.last().unwrap();
+            for difference in differences.iter_mut().rev().skip(1) {
+                last = *difference - last;
+                *difference = last;
+            }
+        }
+
+        sum += differences.iter().fold(0, |acc, &x| x - acc);
+    }
+
+    Ok(sum)
+}
+
+fn main() -> Result<()> {
+    execute(part1, part2)
+}
